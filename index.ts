09e7d3c88547767eb09edb658248ca290582145d
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2023-11-20 15:35:01 +0100

    Implement for graphjson

--- index.ts
+++ index.ts
@@ -1,13 +1,34 @@
 import build from "pino-abstract-transport";
+import fetch from "node-fetch";
 
-export default function GraphjsonTransport(options?: { destination?: string }) {
-  const destinationFile = options?.destination || process.stdout.fd; // path.join(tmpdir(), "rtmp.logs");
-  console.log("->", destinationFile);
+export default function GraphjsonTransport({
+  apiKey,
+  collection,
+}: {
+  apiKey: string;
+  collection: string;
+}) {
+  console.log({ apiKey, collection });
   return build(
-    function (source) {
-      source.on("data", (d) => {
-        console.log("lOOOG", d);
-      });
+    async function (source) {
+      for await (const obj of source) {
+        const payload = {
+          api_key: apiKey,
+          collection,
+          json: JSON.stringify(obj),
+          timestamp: Math.floor(new Date().getTime() / 1000),
+        };
+
+        try {
+          await fetch("https://api.graphjson.com/api/log", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(payload),
+          });
+        } catch (error) {
+          console.error(error);
+        }
+      }
     },
     {
       async close(err) {
